2f646f1bc245b04a6af359ebc5c211caa7478a1e
bourrasque <stampixel.t@gmail.com>
2023-06-24 16:07:27 -0400

    Origins

--- server/server.py
+++ server/server.py
@@ -24,7 +24,9 @@ origins = [
     "http://localhost:3000",
     "localhost:3000",
     "https://triptrove.vercel.app",
+    "http://triptrove.vercel.app",
     "https://triptrove.tech",
+    "http://triptrove.tech"
 ]
 
 app.add_middleware(
@@ -32,7 +34,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
 
 BOOKING_URL = "https://www.booking.com/dml/graphql"
